3ff44c740766ebfe712eceaeef3ac7482078eacd
Jxiang-jc <zjx1020645@163.com>
2019-06-30 17:43:17 +0800

    fix bug

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Typescript-study
+
+- 在`vscode`中。会出现一个`bug`. 当 `tsc xx.ts`之后。会报 `无法重新声明块范围变量'x'`的错误。
+
+> 解决办法: 在项目根目录添加配置文件tsconfig.json。
--- seventh-lesson/app.js
+++ seventh-lesson/app.js
@@ -11,3 +11,23 @@ if (typeof checkType === "string") {
 else {
     console.log('类型不是number');
 }
+// null 和 undefined
+var myNull = null;
+myNull = undefined;
+// never never类型是任何类型的子类型，也可以赋值给任何类型，然而，没有类型是never的子类型或可以赋值给never类型（除了never本身）。即使any也不可以赋值给never。通常表现为抛出异常或无法执行到终止点（例如无线循环）
+var x;
+// 不能将其他类型 转换为 never类型
+// x = 123;
+// never 的应用场景
+// 抛出异常
+function error(message) {
+    throw new Error(message);
+}
+// 死循环
+function loop() {
+    while (true) { }
+}
+var y;
+y = (function () {
+    throw new Error('message');
+})();
