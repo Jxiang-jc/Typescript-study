f67f62957335e5b700819c3230cafaa6a16de4ba
Jxiang-jc <zjx1020656@163.com>
2019-06-30 15:39:34 +0800

    third lesson

---
+++ third-lesson/app.js
@@ -0,0 +1,17 @@
+// 基本数据类型
+// let num = 25;
+// let flo = 25.5;
+// let hex = 0xf000;
+// let binary = 0b1001;
+// let octal = 0o733;
+// 重新赋值
+// num = '25'; // 不能存储非原有的类型数据
+// num = 25.5;
+// ts 原型
+var num = 25;
+// 等同于
+// let num: number = 25;
+// boolean
+var isLogin = false; // let isLogin: boolean = false;
+// 更改
+// isLogin = 1; // 不行
