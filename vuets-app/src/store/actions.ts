103d59c4686138b23fc44ae370bc07484bb47289
Jxiang-jc <zjx1020645@163.com>
2019-08-27 09:49:29 +0800

    配置vuex

---
+++ vuets-app/src/store/actions.ts
@@ -0,0 +1,14 @@
+import { ActionTree } from 'vuex';
+import jwt_decode from 'jwt-decode';
+// import { asyncRouterMap } from '../router';
+
+const actions: ActionTree<any, any> = {
+    async setUser({ state, commit}, user: any) {
+        const decoded: any = jwt_decode(user);
+        commit('SET_USER', decoded);
+        const { key } = decoded;
+        // 
+    }
+}
+
+export default actions;
