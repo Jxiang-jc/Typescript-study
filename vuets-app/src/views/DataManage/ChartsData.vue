a1ca106d81ed71075389fa2c53879eb5f724dbe1
Jxiang-jc <zjx1020645@163.com>
2019-08-29 16:18:19 +0800

    add Chart

--- vuets-app/src/views/DataManage/ChartsData.vue
+++ vuets-app/src/views/DataManage/ChartsData.vue
@@ -1,13 +1,34 @@
 <template>
-    <div class="charts-data">ChartsData</div>
+    <div class="charts-data">
+        <el-tabs type="border-card" v-model="tabPosition">
+            <el-tab-pane label="折线图" name="line">
+                <charts :chartData="chartData" chartType="line"></charts>
+            </el-tab-pane>
+            <el-tab-pane label="柱状图" name="bar">
+                <charts :chartData="chartData" chartType="bar"></charts>
+            </el-tab-pane>
+            <el-tab-pane label="饼图" name="pie">
+                <charts :chartData="chartData" chartType="pie"></charts>
+            </el-tab-pane>
+        </el-tabs>
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Provide } from 'vue-property-decorator';
+import Charts from '@/components/Charts.vue';
 @Component({
-    components: {}
+    components: {
+        Charts
+    }
 })
-export default class ChartsData extends Vue {}
+export default class ChartsData extends Vue {
+    @Provide() tabPosition: string = 'line';
+    @Provide() chartData: any = {
+        xAxisData: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+        yAxisData: [820, 949, 899, 944, 1290, 1330, 1290]
+    };
+}
 </script>
 
 <style lang="scss" scoped>
